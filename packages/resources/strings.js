03e967f3b8f5f5f712c2713d11b5b9f442ecf2ed
kencrawford <kmcrawford@gmail.com>
2017-10-20 07:42:24 -0400

    Initial check-in

---
+++ packages/resources/strings.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  errors: {
+    generic: 'An error occurred: {0}',
+    STORAGE: {
+      DIR_NOT_FOUND: 'Directory "{0}" not found',
+      DIR_NOT_FOUND_CODE: 'dir_not_found',
+      FILE_NOT_FOUND: 'File "{0}" not found',
+      FILE_NOT_FOUND_CODE: 'file_not_found',
+      FILE_NOT_VALID: 'File "{0}" not valid',
+      FILE_NOT_VALID_CODE: 'file_not_valid'
+    }
+  }
+};
---
+++ packages/s3/__test__/s3.test.js
@@ -0,0 +1,20 @@
+const s3 = require('../s3');
+
+const options = {
+  bucket: 'test',
+  region: 'region-test',
+  key: 'test-key',
+  secret: 'test-secret'
+};
+
+test('validate valid conf', () => {
+  const client = new s3(options);
+  expect(client.isValid()).toBe(true);
+});
+
+test('validate missing bucket conf', () => {
+  const missingBucketOptions = options;
+  missingBucketOptions.bucket = undefined;
+  const client = new s3(missingBucketOptions);
+  expect(client.isValid()).toBe(false);
+});
