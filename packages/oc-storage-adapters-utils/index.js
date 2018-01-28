5fd952ef2df61131ec5b159500e33dc1407dd0e5
Nick Balestra <nbalestra@opentable.com>
2018-01-28 12:16:24 +0000

    publish modules with licenses and  without tests

--- packages/oc-storage-adapters-utils/index.js
+++ packages/oc-storage-adapters-utils/index.js
@@ -1,7 +1,7 @@
-const getFileInfo = require('./get-file-info');
-const getNextYear = require('./get-next-year');
-const getMimeType = require('./get-mime-type');
-const strings = require('./strings');
+const getFileInfo = require('./lib/get-file-info');
+const getNextYear = require('./lib/get-next-year');
+const getMimeType = require('./lib/get-mime-type');
+const strings = require('./lib/strings');
 module.exports = {
   getNextYear,
   getFileInfo,
